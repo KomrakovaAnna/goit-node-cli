7ba617268bd61837f4f7b16cf0c4bb497c5aa335
Anna Komrakova <komrakova.anna@gmail.com>
2025-04-18 21:31:26 +0100

    hw1

---
+++ contacts.js
@@ -0,0 +1,39 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+import { nanoid } from "nanoid";
+
+const contactsPath = path.resolve("db", "contacts.json");
+const updateContacts = (contacts) =>
+  fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+export const listContacts = async () => {
+  const data = await fs.readFile(contactsPath, "utf-8");
+  return JSON.parse(data);
+};
+export const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find((contact) => contact.id === contactId);
+  return result || null;
+};
+export async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const contact = contacts.findIndex((contact) => contact.id === contactId);
+  if (contact === -1) return null;
+  const [result] = contacts.splice(contact, 1);
+  await updateContacts(contacts);
+  return result;
+}
+
+export async function addContact(name, email, phone) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+  return newContact;
+}
